bd4e409aa884f08da1e558dffd03451e3fcddab0
MidKnight <35759490+MidKnightXI@users.noreply.github.com>
2021-10-23 17:45:47 +0200

    filling gaps

--- src/api_calls/login_call.js
+++ src/api_calls/login_call.js
@@ -9,16 +9,22 @@ import { authorize } from 'react-native-app-auth';
 
 // base config
 const config = {
-  issuer: '<YOUR_ISSUER_URL>',
-  clientId: '<YOUR_CLIENT_ID>',
-  redirectUrl: '<YOUR_REDIRECT_URL>',
-  scopes: ['<YOUR_SCOPE_ARRAY>'],
-};
+    redirectUrl: 'com.EgoApp://oauth2redirect/reddit',
+    clientId: '<b56Jin3B82dprZpH7E1j3A>',
+    clientSecret: '', // empty string - needed for iOS
+    scopes: ['identity'],
+    serviceConfiguration: {
+      authorizationEndpoint: 'https://www.reddit.com/api/v1/authorize.compact',
+      tokenEndpoint: 'https://www.reddit.com/api/v1/access_token',
+    },
+    customHeaders: {
+      token: {
+        Authorization: 'Basic <base64encoded clientID:YjU2SmluM0I4MmRwclpwSDdFMWozQQo=>',
+      },
+    },
+  };
 
 // use the client to make the auth request and receive the authState
-try {
-  const result = await authorize(config);
-  // result includes accessToken, accessTokenExpirationDate and refreshToken
-} catch (error) {
-  console.log(error);
-}
+const result = await authorize(config);
+
+export default result;
