464c0f5384e055758acda054c380cca6463f2c24
MidKnight <35759490+MidKnightXI@users.noreply.github.com>
2021-10-25 22:59:15 +0200

    adding multi page navigation

--- src/App.js
+++ src/App.js
@@ -7,25 +7,42 @@
  */
 
  import * as React from 'react';
- import { View, Text } from 'react-native';
+ import { Button, View, Text } from 'react-native';
  import { NavigationContainer } from '@react-navigation/native';
  import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>this is your homescreen</Text>
-      </View>
-    );
-  }
+ function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
   
   const Stack = createNativeStackNavigator();
   
+  function DetailsScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details</Text>
+        <Button
+          title="Go to Homescreen"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+    );
+  }
+
   function App() {
     return (
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} options={{title:'overview'}}/>
+          <Stack.Screen name="Details" component={DetailsScreen}/>
         </Stack.Navigator>
       </NavigationContainer>
     );
