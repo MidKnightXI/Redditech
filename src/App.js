ddac672a8a9bfd98debc40849dfcca149a3fcb11
MidKnight <35759490+MidKnightXI@users.noreply.github.com>
2021-10-25 23:19:09 +0200

    adding more buttons

--- src/App.js
+++ src/App.js
@@ -30,9 +30,15 @@
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details</Text>
         <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+        />
+        <Button
           title="Go to Homescreen"
           onPress={() => navigation.navigate('Home')}
         />
+        <Button title="Go back" onPress={() => navigation.goBack()}/>
+        <Button title="Go back to first screen in stack" onPress={() => navigation.popToTop()}/>
       </View>
     );
   }
