d8a6bab2e844ab2f6b38ceaf969ca6b3342bc1b0
MidKnight <35759490+MidKnightXI@users.noreply.github.com>
2021-10-31 15:49:32 +0100

    adding bottom navigation tab + material icons

--- sources/App.js
+++ sources/App.js
@@ -1,13 +1,13 @@
 import * as React from 'react'
-import { Button, View, Text } from 'react-native'
+import { Image, Button, View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { AddToHomeScreen } from '@material-ui/icons';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text style={{color:'black', fontWeight:'bold'}}>Home Screen</Text>
     </View>
   );
 }
@@ -28,16 +28,47 @@ function MessageScreen() {
   );
 }
 
-const Stack = createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={HomeScreen}/>
-        <Stack.Screen name='Profile' component={ProfileScreen}/>
-        <Stack.Screen name='Message' component={MessageScreen}/>
-      </Stack.Navigator>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        activeColor="#e91e63"
+        barStyle={{ backgroundColor: 'tomato' }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={22} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Messages"
+          component={MessageScreen}
+          options={{
+            tabBarLabel: 'Messages',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="message-text-outline" color={color} size={22} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: 'Profile',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account" color={color} size={22} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
