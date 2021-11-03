960f8e4629254ae113eb185c6a0b649114a5c4d9
MidKnight <35759490+MidKnightXI@users.noreply.github.com>
2021-11-03 17:55:58 +0100

    adding setting screen

--- sources/App.js
+++ sources/App.js
@@ -3,6 +3,7 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import HomeScreen from './components/home/home';
 import ProfileScreen from './components/profile/profile'
@@ -15,44 +16,46 @@ const Tab = createMaterialBottomTabNavigator();
 function App() {
   return (
     <ProvideAuth>
-      <NavigationContainer>
-        <Tab.Navigator
-          initialRouteName="Feed"
-          activeColor="#000000"
-          barStyle={{ backgroundColor: 'darkorange'}}
-        >
-          <Tab.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{
-              tabBarLabel: 'Home',
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="home" color={color} size={22} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Messages"
-            component={MessageScreen}
-            options={{
-              tabBarLabel: 'Messages',
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="message-processing-outline" color={color} size={22} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Profile"
-            component={ProfileScreen}
-            options={{
-              tabBarLabel: 'Profile',
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="account" color={color} size={22} />
-              ),
-            }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Tab.Navigator
+            initialRouteName="Home"
+            activeColor="#000000"
+            barStyle={{ backgroundColor: 'darkorange'}}
+          >
+            <Tab.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                tabBarLabel: 'Home',
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="home" color={color} size={22} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Messages"
+              component={MessageScreen}
+              options={{
+                tabBarLabel: 'Messages',
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="message-processing-outline" color={color} size={22} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Profile"
+              component={ProfileScreen}
+              options={{
+                tabBarLabel: 'Profile',
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="account" color={color} size={22} />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </ProvideAuth>
   );
 }
--- sources/components/profile/profile.js
+++ sources/components/profile/profile.js
@@ -1,27 +1,51 @@
 import { View, Text, ScrollView, Button } from 'react-native'
 import * as React from 'react'
+import { Icon } from 'react-native-elements'
 
 import { useAuth } from '../../use-auth';
 
 export default function ProfileScreen() {
-    const clientStatus = useAuth();
-    if (clientStatus.isSignIn === true) {
-      return (
-        <ScrollView>
-          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Profile Screen</Text>
-          </View>
-        </ScrollView>
-      );
-    } else {
-      return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Button
-            onPress={clientStatus.signin}
-            title='Login with Reddit'
-            accessibilityLabel='To access to your profile click on this button'
-            color='#ff4500'/>
+  const clientStatus = useAuth();
+  const [renderParam, setRenderParam] = React.useState(false);
+  if (clientStatus.isSignIn === true && renderParam === false) {
+    return (
+      <View>
+        <View style={{flexDirection: "row", padding: 10, height: 85, margin: 10}}>
+          <Icon
+            size={36}
+            name='gear'
+            type='evilicon'
+            onPress={()=>setRenderParam(true)}
+          />
+          <Text> this is setting button</Text>       
         </View>
-      )
-    }
+      </View>
+    );
+  } else if (clientStatus.isSignIn === true && renderParam === true) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Icon
+          size={36}
+          name='arrow-left'
+          type='evilicon'
+          onPress={()=>setRenderParam(false)}
+        />
+        <ScrollView>
+          <Text>
+            this is some text
+          </Text>
+        </ScrollView>
+      </View>
+    );
+  } else {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+          onPress={clientStatus.signin}
+          title='Login with Reddit'
+          accessibilityLabel='To access to your profile click on this button'
+          color='#ff4500'/>
+      </View>
+    );
   }
+}
