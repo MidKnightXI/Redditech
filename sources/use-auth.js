9aa7b3963ad99a8fd7d26e1a8688566a273b87d3
MidKnightXI <romain.bouguet@epitech.eu>
2021-12-20 17:42:21 +0100

    deleting debuging console.log

--- sources/use-auth.js
+++ sources/use-auth.js
@@ -36,7 +36,6 @@ function useProvideAuth() {
     };
     try {
       const tmp = await authorize(config);
-      console.log(tmp.accessToken)
       setToken(tmp.accessToken);
       setIsSignIn(true);
       return tmp;
@@ -53,10 +52,9 @@ function useProvideAuth() {
       headers: url.includes('oauth') ? {"Authorization": "bearer " + token} : undefined,
       "User-agent": "Ego",
     }
-    console.log('Fetching on ' + url + ' with ' + token)
+    console.log('Fetching on ' + url)
     const res = await fetch(url, config)
     const data = await res.json()
-    console.log(data)
     if (data.error)
       throw data.message;
     return data;
